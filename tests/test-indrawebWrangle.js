8d98f8453679aeb5104e9b6383c1df5459660333
Shaun Nielsen <shaunson26@gmail.com>
2025-02-04 12:20:38 +0100

    Update and improve some testing examples

--- tests/test-indrawebWrangle.js
+++ tests/test-indrawebWrangle.js
@@ -1,12 +1,41 @@
-import { iww } from '../functions/backend/indrawebWrangle.js'
-import AnnualProjectionsExample  from '../data/AnnualProjectionsExample.json' assert { type: 'json' }
-import AnnualObservationsExample  from '../data/AnnualObservationsExample.json' assert { type: 'json' }
+// run using: node .\tests\test-indraewebWrangle
 
-console.log(iww.averageProjections(AnnualProjectionsExample))
-//console.log(iww.summariseYearValues(iww.averageProjections(AnnualProjectionsExample)))
-//console.log(iww.summariseYearValues(AnnualObservationsExample.data))
+import fs from 'fs'; // for data import
+import { iww } from '../assets/backend/indrawebWrangle.js' // the function we're testing
 
-//let aa = AnnualObservationsExample.data
-//let bb = iww.averageProjections(AnnualProjectionsExample)
+
+// Setup import of data
+function parseJson(path){
+    let rawData = fs.readFileSync(path)
+    let jsonObj = JSON.parse(rawData)
+    return(jsonObj)
+}
+
+let data = {
+    yearObservtions: parseJson('data/test-data/getAnnualObservations.json'),
+    yearProjections: parseJson('data/test-data/getAnnualProjections.json')
+}
+
+//console.log(data)
+
+//console.log('Mean of observation years:', iww.summariseYearValues(data.yearObservtions.data))
+//console.log(iww.averageProjections(data.yearProjections))
+//console.log(iww.summariseYearValues(iww.averageProjections(data.yearProjections)))
+
+// Testing
+let result
+result = iww.summariseYearValues(data.yearObservtions.data)
+console.log(result == '14.0')
+
+result = iww.averageProjections(data.yearProjections)
+console.log(
+    result[0].year == '2036',
+    result[0].value == '15.4',
+    result[0].rcp == 'rcp45',
+)
+
+result = iww.summariseYearValues(iww.averageProjections(data.yearProjections))
+console.log(result == '15.2')
+
 
 
